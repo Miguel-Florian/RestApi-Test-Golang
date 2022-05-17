df7ff4a39b14e7e90531f2545f14332e8e86e509
Miguel-Florian <migflor04@gmail.com>
2022-05-17 10:08:15 +0100

    Create database instace

--- Config/database.go
+++ Config/database.go
@@ -1,21 +1,41 @@
 package config
 
-//go get go.mongodb.org/mongo-driver/mongo
-//dep ensure -add "go.mongodb.org/mongo-driver/mongo"
-/*import (
-    "context"
-    "time"
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
 
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
-    "go.mongodb.org/mongo-driver/mongo/readpref"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-defer cancel()
-client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
-defer func() {
-    if err = client.Disconnect(ctx); err != nil {
-        panic(err)
-    }
-}()*/
+func ConnectDB() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017/"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//ping the database
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB")
+	return client
+}
+
+//Client instance
+var DB *mongo.Client = ConnectDB()
+
+//getting database collections
+func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	collection := client.Database("ebook").Collection(collectionName)
+	return collection
+}
