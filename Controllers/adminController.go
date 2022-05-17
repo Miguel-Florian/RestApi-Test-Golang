3ed25d67eb56f61cad5e36cd9da00a7f20d3907d
Miguel-Florian <migflor04@gmail.com>
2022-05-17 10:13:21 +0100

    adding and updating users and book controllers

---
+++ Controllers/adminController.go
@@ -0,0 +1,17 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func CreateAdmin(c *gin.Context) {
+	return
+}
+func LoginAdmin(c *gin.Context) {
+	return
+
+}
+func Dashboard(c *gin.Context) {
+	return
+
+}
--- Controllers/bookController.go
+++ Controllers/bookController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	//"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func CreateBook(c *gin.Context) {
