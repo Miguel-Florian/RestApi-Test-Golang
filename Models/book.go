d5343e2271c25d34c6ab2b03aa5498e163f1abbc
Miguel-Florian <migflor04@gmail.com>
2022-06-09 06:10:02 +0100

    Updating json description of description fiel on book model

--- Models/book.go
+++ Models/book.go
@@ -5,8 +5,8 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Book struct {
 	ID          primitive.ObjectID `json:"_id,onitempty" bson:"_id,onitempty"`
 	Title       string             `json:"title,onitempty" bson:"title,onitempty"`
-	Description string             `json:"description" bson:"title,descritpion"`
+	Description string             `json:"description" bson:"description,descritpion"`
 	DateSortie  string             `json:"date sortie,omitempty" bson:"date sortie,omitempty"`
 	Auteur      []string           `json:"auteurs,onitempty" bson:"auteurs,onitempty"`
-	Catégories  []string           `json:"categories,onitempty" bson:"categories,onitempty"`
+	Categories  []string           `json:"categories,onitempty" bson:"categories,onitempty"`
 }
