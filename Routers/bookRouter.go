bc4950bdf885dcf998cf2955f8f951696114a839
Miguel-Florian <migflor04@gmail.com>
2022-06-09 07:33:04 +0100

    Updating Book routers

--- Routers/bookRouter.go
+++ Routers/bookRouter.go
@@ -8,8 +8,8 @@ import (
 func BookRoute(router *gin.Engine) {
 	//All routes related to book comes here
 	router.POST("/book/register", controllers.CreateBook())
-	router.PUT("/book/:id", controllers.UpdateBook)
-	router.GET("/book/:id", controllers.GetBookById)
-	router.GET("/books", controllers.GetAllBooks)
-	router.DELETE("/book/:id", controllers.DeleteBook)
+	router.PUT("/book/:id", controllers.UpdateBook())
+	router.GET("/book/:id", controllers.GetBookById())
+	router.GET("/books", controllers.GetAllBooks())
+	router.DELETE("/book/:id", controllers.DeleteBook())
 }
