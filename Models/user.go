3d6edc2490fd2722866e7100c3a0cfbe2668d9e5
Miguel-Florian <migflor04@gmail.com>
2022-06-10 11:50:25 +0100

    Updating user model

--- Models/user.go
+++ Models/user.go
@@ -1,12 +1,15 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type User struct {
 	ID        primitive.ObjectID `json:"_id,onitempty" bson:"_id,onitempty"`
+	Username  string             `json:"username,onitempty" bson:"username,onitempty"`
 	FirstName string             `json:"firstname,onitempty" bson:"firstname,onitempty"`
 	LastName  string             `json:"lastname,onitempty" bson:"lastname,onitempty"`
-	Email     string             `json:"email,onitempty" bson:"email,onitempty"`
+	Email     string             `json:"email,onitempty" bson:"email,onitempty,unique"`
 	Password  string             `json:"password,onitempty" bson:"password,onitempty"`
 	//HashPassword []byte        `json:"hashpassword,omitempty "`
 }
