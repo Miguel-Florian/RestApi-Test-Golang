64f343e7364774830ca1b98db18f7e4fb0959864
Miguel-Florian <migflor04@gmail.com>
2022-06-14 21:35:38 +0100

    Grouping route with api prefix

--- Routers/bookRouter.go
+++ Routers/bookRouter.go
@@ -7,9 +7,14 @@ import (
 
 func BookRoute(router *gin.Engine) {
 	//All routes related to book comes here
-	router.POST("/book/register", controllers.CreateBook())
-	router.PUT("/book/:id", controllers.UpdateBook())
-	router.GET("/book/:id", controllers.GetBookById())
-	router.GET("/books", controllers.GetAllBooks())
-	router.DELETE("/book/:id", controllers.DeleteBook())
+
+	api := router.Group("/api/book")
+	{
+		api.POST("/register", controllers.CreateBook())
+		api.PUT("/:id", controllers.UpdateBook())
+		api.GET("/:id", controllers.GetBookById())
+		api.GET("/books", controllers.GetAllBooks())
+		api.DELETE("/:id", controllers.DeleteBook())
+	}
+
 }
