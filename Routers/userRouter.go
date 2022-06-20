8a4ec4d5e7dcc19adbcc9abfe334680d03c1f232
Miguel-Florian <migflor04@gmail.com>
2022-06-20 14:16:15 +0100

    updating

--- Routers/userRouter.go
+++ Routers/userRouter.go
@@ -13,6 +13,7 @@ func UserRoute(router *gin.Engine) {
 		api.POST("/save", controllers.CreateUser())
 		api.POST("/login", controllers.LoginUser())
 		api.POST("/register", controllers.RegisterUser())
+		api.GET("/auth", controllers.ValidateToken())
 		api.POST("/logout", controllers.LogoutUser())
 		api.GET("/:id", controllers.GetUserById())
 		api.PUT("/update/:id", controllers.UpdateUserById())
